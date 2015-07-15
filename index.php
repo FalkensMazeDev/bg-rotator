1b3435b6ab8672abcc28aa4eee2ae36f1f6e3f78
Greg the WP CMS Ninja <greg@wpcms.ninja>
2015-07-15 14:26:09 -0400

    Modified ids to control display none declarations instead of class. also moved script to bottom of page instead of in head.

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ $bgcount = count($bgimages);
 			position: absolute;
 			width:100%;
 			height:100%;
-			display:none;
 			top:0px;
 			left:0px;
 			z-index:-9999;
@@ -39,7 +38,11 @@ if ($bgcount > 0) {
 			-moz-background-size: cover;
 			-o-background-size: cover;
 			background-size: cover;
-			<?php if ($bgid == 0) echo "display:block;"; ?>
+			<?php 
+			if ($bgid == 0) echo "display:block;";
+			else echo "display:none;";
+			
+			?>
 		}
 		<?php
 	}
@@ -54,40 +57,7 @@ if ($bgcount > 0) {
 			padding:20px;
 		}
 		</style>
-		<script>
-		jQuery( document ).ready(function() {
-			var imgcnt = <?=$bgcount;?>;
-			var curimage = 0;
-			var rotatorCall;
-			var fadetime = 1000;
-			var waittime = 3000;
-			var stopBgImages = false;
-			
-			var changeBgImage = function () {
-				//console.log("stop bg? " + stopBgImages);
-				if (!stopBgImages) {
-				    var newimg;
-				    if (curimage == (imgcnt-1)) newimg = 0;
-				    else newimg = curimage + 1;
-
-				    jQuery("#image" + curimage).fadeToggle(fadetime);
-				    jQuery("#image" + newimg).fadeToggle(fadetime);
-					jQuery("#image" + newimg).promise().done(function(){
-						rotatorCall = setTimeout(function(){ changeBgImage()}, waittime);
-					});
-					curimage = newimg;
-				}
-			}
-			
-			rotatorCall = setTimeout(function(){ changeBgImage()}, waittime);
-			
-			jQuery(".stopBgImages").click(function(){
-				stopBgImages = true;
-				//console.log("attemptstop");
-			});
-
-		});
-		</script>
+		
 	</head>
 	<body>
 <?php
@@ -110,7 +80,47 @@ if ($bgcount > 0)
 			</p>
 			<p>
 				<a href="#" class="stopBgImages">Stop Rotating</a>
+				<a href="#" class="startBgImages">Start Rotating</a>
 			</p>
 		</div>
 </body>
+<script>
+		jQuery( document ).ready(function() {
+			var imgcnt = <?=$bgcount;?>;
+			var curimage = 0;
+			var rotatorCall;
+			var fadetime = 1000;
+			var waittime = 3000;
+			var stopBgImages = false;
+			
+			var changeBgImage = function () {
+				if (!stopBgImages) {
+				    var newimg;
+				    if (curimage == (imgcnt-1)) newimg = 0;
+				    else newimg = curimage + 1;
+
+				    jQuery("#image" + curimage).fadeToggle(fadetime);
+				    jQuery("#image" + newimg).fadeToggle(fadetime);
+					jQuery("#image" + newimg).promise().done(function(){
+						rotatorCall = setTimeout(function(){ changeBgImage()}, waittime);
+					});
+					curimage = newimg;
+				}
+			}
+			
+			rotatorCall = setTimeout(function(){ changeBgImage()}, waittime);
+			
+			jQuery(".stopBgImages").click(function(){
+				stopBgImages = true;
+				//console.log("attemptstop");
+			});
+			
+			jQuery(".startBgImages").click(function(){
+				stopBgImages = false;
+				changeBgImage();
+				//console.log("attemptstop");
+			});
+			
+		});
+</script>
 </html>
